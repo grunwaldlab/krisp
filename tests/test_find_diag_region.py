ef8791cb5879e172f2dc1e53dd56c3909b20da4f
Ali Tavallaie <a.tavallaie@gmail.com>
2024-08-28 22:32:27 +0330

    chore(tests): fix imports

--- tests/test_find_diag_region.py
+++ tests/test_find_diag_region.py
@@ -1,19 +1,22 @@
-from krisp_vcf.find_diag_var import *
-from krisp_vcf.find_diag_var import _parse_group_data, _check_variant, GroupedVariant
-from krisp_vcf.find_diag_region import GroupedRegion, find_diag_region
+from krisp.krisp_vcf.find_diag_var import (
+    _parse_group_data,
+    GroupedVariant,
+)
+from krisp.krisp_vcf import GroupedRegion, find_diag_region
 import pysam
 from itertools import islice
 import unittest
-from Bio import SeqIO
 
 
 class TestGroupedRegion(unittest.TestCase):
     def setUp(self):
         pysam.set_verbosity(0)
-        self.vcf = pysam.VariantFile('test_data/unfilt_allscafs_n666.vcf.gz')
-        self.groups = _parse_group_data('test_data/test_metadata.tsv')
-        self.groups = {g: v for g, v in self.groups.items() if g in ["NA1", "NA2", "EU1", "EU2"]}
-        self.ref = 'test_data/PR-102_v3.1.fasta'
+        self.vcf = pysam.VariantFile("test_data/unfilt_allscafs_n666.vcf.gz")
+        self.groups = _parse_group_data("test_data/test_metadata.tsv")
+        self.groups = {
+            g: v for g, v in self.groups.items() if g in ["NA1", "NA2", "EU1", "EU2"]
+        }
+        self.ref = "test_data/PR-102_v3.1.fasta"
         self.variant1 = next(self.vcf)
         self.variant2 = next(self.vcf)
         self.variant3 = next(self.vcf)
@@ -21,7 +24,11 @@ class TestGroupedRegion(unittest.TestCase):
         self.vcf_subset = islice(self.vcf, 10, 15)
 
     def test_init(self):
-        x = GroupedRegion(GroupedVariant.from_vcf(self.vcf_subset, groups=self.groups), group="NA1", reference=self.ref)
+        x = GroupedRegion(
+            GroupedVariant.from_vcf(self.vcf_subset, groups=self.groups),
+            group="NA1",
+            reference=self.ref,
+        )
         print(x.region_length())
 
     # def test_sliding_window(self):
@@ -34,7 +41,4 @@ class TestGroupedRegion(unittest.TestCase):
     #         print(len(x.variants))
 
     def test_find_diag_region(self):
-        find_diag_region(self.vcf,
-                         groups=self.groups,
-                         reference=self.ref)
-
+        find_diag_region(self.vcf, groups=self.groups, reference=self.ref)
--- tests/test_find_diag_var.py
+++ tests/test_find_diag_var.py
@@ -1,17 +1,20 @@
-from diagvar.find_diag_var import *
-from diagvar.find_diag_var import _parse_group_data, _check_variant, GroupedVariant
+from krisp.krisp_vcf.find_diag_var import (
+    _parse_group_data,
+    GroupedVariant,
+)
 import pysam
 from itertools import islice
 import unittest
 
 
 class TestGroupedVariant(unittest.TestCase):
-    
     def setUp(self):
         pysam.set_verbosity(0)
-        self.vcf = pysam.VariantFile('test_data/unfilt_allscafs_n666.vcf.gz')
-        self.groups = _parse_group_data('test_data/test_metadata.tsv')
-        self.groups = {k: v for k, v in self.groups.items() if k in ['NA1','NA2', 'EU1', 'EU2']}
+        self.vcf = pysam.VariantFile("test_data/unfilt_allscafs_n666.vcf.gz")
+        self.groups = _parse_group_data("test_data/test_metadata.tsv")
+        self.groups = {
+            k: v for k, v in self.groups.items() if k in ["NA1", "NA2", "EU1", "EU2"]
+        }
         self.variant1 = GroupedVariant(next(self.vcf), self.groups)
         self.variant2 = GroupedVariant(next(self.vcf), self.groups)
         self.variant3 = GroupedVariant(next(self.vcf), self.groups)
@@ -19,20 +22,28 @@ class TestGroupedVariant(unittest.TestCase):
         self.vcf_subset = islice(self.vcf, 10, 15)
 
     def test_init(self):
-        self.assertEqual(self.diag_var.sample_counts,
-                         {'NA1': 222, 'EU1': 202, 'EU2': 0, 'NA2': 18})
-        self.assertEqual(self.diag_var.allele_counts,
-                         {'NA1': {'T': 222}, 'EU1': {'C': 202}, 'EU2': {}, 'NA2': {'C': 18}})
-        self.assertEqual(self.diag_var.conserved,
-                         {'NA1': 'T', 'EU1': 'C', 'EU2': None, 'NA2': 'C'})
-        self.assertEqual(self.diag_var.diagnostic,
-                         {'NA1': None, 'EU1': None, 'EU2': None, 'NA2': None})
+        self.assertEqual(
+            self.diag_var.sample_counts, {"NA1": 222, "EU1": 202, "EU2": 0, "NA2": 18}
+        )
+        self.assertEqual(
+            self.diag_var.allele_counts,
+            {"NA1": {"T": 222}, "EU1": {"C": 202}, "EU2": {}, "NA2": {"C": 18}},
+        )
+        self.assertEqual(
+            self.diag_var.conserved, {"NA1": "T", "EU1": "C", "EU2": None, "NA2": "C"}
+        )
+        self.assertEqual(
+            self.diag_var.diagnostic,
+            {"NA1": None, "EU1": None, "EU2": None, "NA2": None},
+        )
 
     def test_from_vcf(self):
         gen = GroupedVariant.from_vcf(self.vcf_subset, groups=self.groups)
-        self.assertEqual(next(gen).allele_counts,
-                         {'NA1': {'T': 236}, 'EU1': {'C': 207}, 'EU2': {}, 'NA2': {'C': 22}})
+        self.assertEqual(
+            next(gen).allele_counts,
+            {"NA1": {"T": 236}, "EU1": {"C": 207}, "EU2": {}, "NA2": {"C": 22}},
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
